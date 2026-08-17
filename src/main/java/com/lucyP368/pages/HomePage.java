da15fe91b7b5ffd78b0bcc3135b5ae0dcbf45565
lucyP368 <lucy.patton.368@gmail.com>
2026-08-17 15:51:28 -0400

    Removed build artifacts and other unneeded files

---
+++ src/main/java/com/lucyP368/pages/HomePage.java
@@ -0,0 +1,12 @@
+package com.lucyP368.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends Page {
+    public HomePage(WebDriver driver) {
+        super(driver, List.of());
+    }
+    
+}
